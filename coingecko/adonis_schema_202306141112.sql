9972788bbe053050e0e927fb101f2a4273257f6d
Aatir1224 <aatirqureshi1224@gmail.com>
2023-06-14 11:13:41 +0530

    added database exports

---
+++ coingecko/adonis_schema_202306141112.sql
@@ -0,0 +1,2 @@
+INSERT INTO public.adonis_schema ("name",batch,migration_time) VALUES
+	 ('database/migrations/1686720137240_coingeckos',1,'2023-06-14 10:54:54.082978+05:30');
---
+++ coingecko/adonis_schema_versions_202306141112.sql
@@ -0,0 +1,2 @@
+INSERT INTO public.adonis_schema_versions ("version") VALUES
+	 (2);
